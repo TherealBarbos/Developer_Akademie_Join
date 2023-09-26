25a2c006b30c51d4df821a92273cd07a27cf9e3b
TherealBarbos <126061412+TherealBarbos@users.noreply.github.com>
2023-09-26 10:07:33 +0200

    #

--- Join/assets/js/board.js
+++ Join/assets/js/board.js
@@ -1,74 +1,88 @@
-let todos = [ { 
-    'id' : '0',
-    'category': 'toDo',
-    'title' : 'Story',
-    'textI' : 'Kochwelt Page & Recipe Recommender',
-    'textII' : 'Build start page with recipe recommendation...',
-    'progressbar' : '',
-    'progress' : '1/2 Subtasks',
-    'badge' : 'assets/img/BSP.png',
-    'prio' : 'assets/img/medium_no_bg.png'
-    },
-    { 
-        'id' : '1',
-        'category': 'done',
-        'title' : 'Story',
-        'textI' : 'Kochwelt Page & Recipe Recommender',
-        'textII' : 'Build start page with recipe recommendation...',
-        'progressbar' : '',
-        'progress' : '1/2 Subtasks',
-        'badge' : 'assets/img/BSP.png',
-        'prio' : 'assets/img/medium_no_bg.png'
-        }
-
+let todos = [
+  {
+    id: "0",
+    category: "toDo",
+    title: "Story",
+    textI: "Kochwelt Page & Recipe Recommender",
+    textII: "Build start page with recipe recommendation...",
+    progressbar: "",
+    progress: "1/2 Subtasks",
+    badge: "assets/img/BSP.png",
+    prio: "assets/img/medium_no_bg.png",
+  },
+  {
+    id: "1",
+    category: "done",
+    title: "BUG?",
+    textI: "Kochwelt Page & Recipe Recommender",
+    textII: "Build start page with recipe recommendation...",
+    progressbar: "",
+    progress: "1/2 Subtasks",
+    badge: "assets/img/BSP.png",
+    prio: "assets/img/medium_no_bg.png",
+  },
+  {
+    id: "2",
+    category: "inProgress",
+    title: "Test",
+    textI: "Kochwelt Page & Recipe Recommender",
+    textII: "Build start page with recipe recommendation...",
+    progressbar: "",
+    progress: "1/2 Subtasks",
+    badge: "assets/img/BSP.png",
+    prio: "assets/img/medium_no_bg.png",
+  },
 ];
 
 let currentDraggedElement;
 
 function updateHTML() {
-    let toDo = todos.filter(t => t['category'] == 'toDo');
+  let toDo = todos.filter((t) => t["category"] == "toDo");
 
-    document.getElementById('toDo').innerHTML = '';
+  document.getElementById("toDo").innerHTML = "";
 
-    for (let index = 0; index < toDo.length; index++) {
-        const element = toDo[index];
-        document.getElementById('toDo').innerHTML += generateTodoCard(element);
-    }
+  for (let index = 0; index < toDo.length; index++) {
+    const element = toDo[index];
+    document.getElementById("toDo").innerHTML += generateTodoCard(element);
+  }
 
-    let inProgress = todos.filter(t => t['category'] == 'inProgress');
+  let inProgress = todos.filter((t) => t["category"] == "inProgress");
 
-    document.getElementById('inProgress').innerHTML = '';   
+  document.getElementById("inProgress").innerHTML = "";
 
-    for (let index = 0; index < inProgress.length; index++) {
-        const element = inProgress[index];
-        document.getElementById('inProgress').innerHTML += generateInProgressCard(element);
-    }
+  for (let index = 0; index < inProgress.length; index++) {
+    const element = inProgress[index];
+    document.getElementById("inProgress").innerHTML +=
+      generateInProgressCard(element);
+  }
 
-    let done = todos.filter(t => t['category'] == 'done');
+  let done = todos.filter((t) => t["category"] == "done");
 
-    document.getElementById('done').innerHTML = '';
+  document.getElementById("done").innerHTML = "";
 
-    for (let index = 0; index < done.length; index++) {
-        const element = done[index];
-        document.getElementById('done').innerHTML += generateDoneCard(element);
-    }
+  for (let index = 0; index < done.length; index++) {
+    const element = done[index];
+    document.getElementById("done").innerHTML += generateDoneCard(element);
+  }
 
-    let awaitFeedback = todos.filter(t => t['category'] == 'awaitFeedback');
+  let awaitFeedback = todos.filter((t) => t["category"] == "awaitFeedback");
 
-    document.getElementById('awaitFeedback').innerHTML = '';
+  document.getElementById("awaitFeedback").innerHTML = "";
 
-    for (let index = 0; index < awaitFeedback.length; index++) {
-        const element = awaitFeedback[index];
-        document.getElementById('awaitFeedback').innerHTML += generateAwaitFeedbackCard(element);
-    }
+  for (let index = 0; index < awaitFeedback.length; index++) {
+    const element = awaitFeedback[index];
+    document.getElementById("awaitFeedback").innerHTML +=
+      generateAwaitFeedbackCard(element);
+  }
 }
 
 function startDregging(id) {
-    currentDraggedElement = id;
+  currentDraggedElement = id;
+  document.getElementById(id).classList.add("cardDragging");
 }
 
 function generateTodoCard(todo) {
-    return ` <div class="card" draggable="true" ondragstart="startDregging('${todo.id}')">
+  return ` <div class="card" draggable="true" ondragstart="startDregging('${todo.id}')">
     <div class="cardFrame">
       <div class="cardLable">${todo.title}</div>
       <div class="cardTextbox">
@@ -91,7 +105,7 @@ function generateTodoCard(todo) {
   </div>`;
 }
 function generateDoneCard(todo) {
-    return ` <div class="card" draggable="true" ondragstart="startDregging('${todo.id}')">
+  return ` <div class="card" draggable="true" ondragstart="startDregging('${todo.id}')">
     <div class="cardFrame">
       <div class="cardLable">${todo.title}</div>
       <div class="cardTextbox">
@@ -114,7 +128,7 @@ function generateDoneCard(todo) {
   </div>`;
 }
 function generateInProgressCard(todo) {
-    return ` <div class="card" draggable="true" ondragstart="startDregging('${todo.id}')">
+  return ` <div class="card" draggable="true" ondragstart="startDregging('${todo.id}')">
     <div class="cardFrame">
       <div class="cardLable">${todo.title}</div>
       <div class="cardTextbox">
@@ -137,7 +151,7 @@ function generateInProgressCard(todo) {
   </div>`;
 }
 function generateAwaitFeedbackCard(todo) {
-    return ` <div class="card" draggable="true" ondragstart="startDregging('${todo.id}')">
+  return ` <div class="card" draggable="true" ondragstart="startDregging('${todo.id}')">
     <div class="cardFrame">
       <div class="cardLable">${todo.title}</div>
       <div class="cardTextbox">
@@ -161,18 +175,19 @@ function generateAwaitFeedbackCard(todo) {
 }
 
 function allowDrop(ev) {
-    ev.preventDefault();
+  ev.preventDefault();
 }
 
 function moveTo(category) {
-    todos[currentDraggedElement]['category'] = category;
-    updateHTML();
+  todos[currentDraggedElement]["category"] = category;
+  updateHTML();
+  unhighlight(category);
 }
 
 function highlight(id) {
-    document.getElementById(id).classList.add('drag-over');
+  document.getElementById(id).classList.add("drag-over");
 }
 
 function unhighlight(id) {
-    document.getElementById(id).classList.remove('drag-over');
+  document.getElementById(id).classList.remove("drag-over");
 }
