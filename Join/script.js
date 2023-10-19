b89331fe3bdc792454c57c3dcff529d1ff1649f4
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2023-10-19 14:55:20 +0200

    logout menu is now set up

--- Join/script.js
+++ Join/script.js
@@ -1,5 +1,5 @@
 let UserFirstLetter = []
-
+let MenuToggle = false;
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -26,4 +26,19 @@ function pasteFirstLetter() {
 
 function getArray(key) {
     return JSON.parse(localStorage.getItem(key));
+}
+
+function displayLogoutMenu() {
+    let menu = document.getElementById('logout-menu');
+    let box = document.getElementById('firstletterbox');
+
+    if (!MenuToggle) {
+        box.classList.add('grey-bg');
+        menu.classList.remove('d-none');
+        MenuToggle = true;
+    } else if (MenuToggle) {
+        box.classList.remove('grey-bg');
+        menu.classList.add('d-none');
+        MenuToggle = false;
+    }
 }
