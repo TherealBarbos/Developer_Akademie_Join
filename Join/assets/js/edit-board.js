24cda9393e28f02922bdb3bef3d891501fb0fc40
OliverHaerle <136490738+OliverHaerle@users.noreply.github.com>
2023-10-25 17:45:08 +0200

    hover effect closing icon

--- Join/assets/js/edit-board.js
+++ Join/assets/js/edit-board.js
@@ -6,7 +6,7 @@ function editTask(id) {
     card.innerHTML = `
 
     <div class="bOverlayClose-edit" onclick="closeOverlay()">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <svg class="close-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
           <mask index="mask0_93030_4211" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
               height="24">
               <rect width="24" height="24" fill="#D9D9D9" />
