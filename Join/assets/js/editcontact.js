f7a2be77ddcd492a6df0a635113f57055ed546b8
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2023-10-29 21:53:57 +0100

    button position fixed

--- Join/assets/js/editcontact.js
+++ Join/assets/js/editcontact.js
@@ -30,6 +30,6 @@ function setContactValues(index) {
     document.getElementById('edit-input-email').value = `${contacts[index]['email']}`;
     document.getElementById('edit-input-phone').value = `${contacts[index]['phone']}`;
     document.getElementById('saveContactValue').innerHTML = /*html*/`
-    <span onclick="saveContactValues(${index})">Save <img src="../img/check.svg" alt="check"></span>
+    <span class="create-btn-align" onclick="saveContactValues(${index})">Save <img src="../img/check.svg" alt="check"></span>
     `
 }
